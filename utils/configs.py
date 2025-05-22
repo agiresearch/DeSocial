10044d7dfbb0c6bc880cc6b04119bf2521ef09df
Luckfort <huangjingyuan5@163.com>
2025-05-22 16:34:32 -0400

    update

--- utils/configs.py
+++ utils/configs.py
@@ -9,52 +9,33 @@ def get_link_prediction_args():
     parser.add_argument('--dataset_name', type=str, help='dataset to be used', default='UCI',
                         choices=['UCI', 'Enron', 'GDELT', 'Memo-Tx'])
     parser.add_argument('--batch_size', type=int, default=1024, help='batch size')
-    parser.add_argument('--model_name', type=str, default='SGC', help='name of the model, note that EdgeBank is only applicable for evaluation',
-                        choices=['GCN', 'GAT', 'SAGE', 'MLP', 'SGC', 'DeSocial'])
+    parser.add_argument('--model_name', type=str, default='SAGE', help='name of the model',
+                        choices=['GCN', 'GAT', 'SAGE', 'MLP', 'SGC'])
     parser.add_argument('--cuda', type=int, default=0, help='number of gpu to use')
     parser.add_argument('--num_neighbors', type=int, default=20, help='number of neighbors to sample for each node')
-    parser.add_argument('--sample_neighbor_strategy', type=str, default='recent', choices=['uniform', 'recent', 'time_interval_aware'], help='how to sample historical neighbors')
-    parser.add_argument('--time_scaling_factor', default=1e-6, type=float, help='the hyperparameter that controls the sampling preference with time interval, '
-                        'a large time_scaling_factor tends to sample more on recent links, 0.0 corresponds to uniform sampling, '
-                        'it works when sample_neighbor_strategy == time_interval_aware')
-    parser.add_argument('--num_walk_heads', type=int, default=8, help='number of heads used for the attention in walk encoder')
     parser.add_argument('--num_heads', type=int, default=2, help='number of heads used in attention layer')
     parser.add_argument('--num_layers', type=int, default=2, help='number of model layers')
-    parser.add_argument('--walk_length', type=int, default=1, help='length of each random walk')
-    parser.add_argument('--time_gap', type=int, default=2000, help='time gap for neighbors to compute node features')
-    parser.add_argument('--time_feat_dim', type=int, default=100, help='dimension of the time embedding')
-    parser.add_argument('--position_feat_dim', type=int, default=172, help='dimension of the position embedding')
-    parser.add_argument('--edge_bank_memory_mode', type=str, default='unlimited_memory', help='how memory of EdgeBank works',
-                        choices=['unlimited_memory', 'time_window_memory', 'repeat_threshold_memory'])
-    parser.add_argument('--time_window_mode', type=str, default='fixed_proportion', help='how to select the time window size for time window memory',
-                        choices=['fixed_proportion', 'repeat_interval'])
-    parser.add_argument('--patch_size', type=int, default=1, help='patch size')
-    parser.add_argument('--channel_embedding_dim', type=int, default=50, help='dimension of each channel embedding')
-    parser.add_argument('--max_input_sequence_length', type=int, default=48, help='maximal length of the input sequence of each node')
     parser.add_argument('--learning_rate', type=float, default=0.001, help='learning rate')
     parser.add_argument('--dropout', type=float, default=0.1, help='dropout rate')
     parser.add_argument('--num_epochs', type=int, default=100, help='number of epochs')
     parser.add_argument('--optimizer', type=str, default='Adam', choices=['SGD', 'Adam', 'RMSprop'], help='name of optimizer')
     parser.add_argument('--weight_decay', type=float, default=0.0, help='weight decay')
     parser.add_argument('--patience', type=int, default=10, help='patience for early stopping')
-    parser.add_argument('--num_runs', type=int, default=5, help='number of runs')
     parser.add_argument('--test_interval_epochs', type=int, default=2, help='how many epochs to perform testing once')
     parser.add_argument('--negative_sample_strategy', type=str, default='random', choices=['random', 'random_with_collision_check', 'historical', 'inductive'],
                         help='strategy for the negative edge sampling')
-    parser.add_argument('--use_feature', type=str, default='', help='whether to use text embeddings as feature') # or Bert
-    parser.add_argument('--load_best_configs', action='store_true', default=False, help='whether to load the best configurations')
+    parser.add_argument('--use_feature', type=str, default='', help='whether to use text embeddings as feature')
     parser.add_argument('--strategy', type=str, default='full', choices=['fine', 'full'], help='fine-tune or full-retrain')
     parser.add_argument('--seed', type=int, default=1, help='random seed')
-    parser.add_argument('--use_pretrained', action='store_true', default=False, help='whether to use pretrained model')
     parser.add_argument('--in_dim', type=int, default=384, help='input dimension')
     parser.add_argument('--hidden_dim', type=int, default=64, help='hidden dimension')
     parser.add_argument('--out_dim', type=int, default=128, help='output dimension')
     parser.add_argument('--start_period', type=int, default=0, help='start time')
     parser.add_argument('--end_period', type=int, default=38, help='end time')
-    parser.add_argument('--name_tag', type=str, default='DeSocial-test', help='name tag (distinguish different experiments)')
-    parser.add_argument('--test_interval_periods', type=int, default=10, help='test interval periods')
+    parser.add_argument('--name_tag', type=str, default='DeSocial-Run', help='name tag (distinguish different experiments)')
+    parser.add_argument('--load_best_configs', action='store_true', default=False, help='whether to load the best configurations')
 
-    ## Multi-Node Consensus Voting
+    ## Multi-Node Decentralized Consensus Voting
     parser.add_argument('--experts', type=int, default=5, help='number of experts')
 
     ## Heuristics Backbone Selections
